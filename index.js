4ce13694f1880f920d7a65f32d7390b16e777e4a
Imhandegbelo <okogbe@outloo.com>
2022-11-05 09:50:03 +0100

    added the delete route

--- index.js
+++ index.js
@@ -10,6 +10,16 @@ app.use(json());
 
 app.use("/", routes);
 
+app.use("/delete:id", (req, res) => {
+	var id = req.params.id
+	var newTicket = ticket.filter( el => el.id !=id)
+	flightTicket = newTicket
+	res.send({
+		success: true;
+		message: "flight ticket successfully deleted"
+	})
+}
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
